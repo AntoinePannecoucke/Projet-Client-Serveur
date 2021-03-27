04009941463a4dbc73316054f451b657219fc04f
Antoine <antoine_pannecoucke@orange.fr>
2021-03-27 13:11:37 +0100

    adapt gitignore and build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,18 @@
+#!/bin/bash
+
+if [ ! -d "build" ]; then
+  mkdir build
+fi
+
+
 #Builds
 g++ -c -Wall src/Socket.cpp -o build/Socket.o
 g++ -c -Wall src/Server.cpp -o build/Server.o
 g++ -c -Wall src/Client.cpp -o build/Client.o
+
+if [ ! -d "exe" ]; then
+  mkdir exe
+fi
 
 #Exe
 g++ -Wall build/Socket.o build/Server.o -o exe/Server
